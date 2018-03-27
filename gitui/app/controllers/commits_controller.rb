63c1a09d669aa79a9c0be0061418455feadb0b6f
Tony Yang <tianyuyang658@yahoo.com>
2018-03-27 15:04:42 -0700

    Make the commits page to properly display the corresponding commit histories of the selected branch

--- gitui/app/controllers/commits_controller.rb
+++ gitui/app/controllers/commits_controller.rb
@@ -1,6 +1,7 @@
 class CommitsController < ApplicationController
   def show
     @repo_name = params[:repo_name]
+    @branch = params[:branch] || 'master'
     repo_url_inside_container = repo_url_in_container_mapping(@repo_name)
 
     @metadata = {}
@@ -11,7 +12,13 @@ class CommitsController < ApplicationController
       unless repo_data.head_unborn?
         @metadata[:local_branches] = repo_data.branches.each_name(:local).sort
 
-        last_commit_sha = repo_data.head.target_id
+        last_commit_sha = nil
+        if 'master' != @branch
+          last_commit_sha = repo_data.branches[@branch].target_id
+        else
+          last_commit_sha = repo_data.head.target_id
+        end
+
         commit_walker = Rugged::Walker.new(repo_data)
         commit_walker.push(last_commit_sha)
         commit_walker.each do |commit|
